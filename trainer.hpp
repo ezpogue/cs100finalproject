5fd1ca6cbf84db04c41c2198e9523aeb99f44cd0
Patelriyaaa <patel.riya1@yahoo.com>
2021-11-15 19:29:13 -0800

    trainer.hpp

---
+++ trainer.hpp
@@ -0,0 +1,26 @@
+#include <string>
+#ifndef __trainer_hpp__
+#define __trainer_hpp__
+
+class Trainer
+    {
+        protected:
+        string name;
+        int items;
+        Pokemon* active;
+        Pokemon team [4];
+
+        public:
+        switchPokemon();
+        
+    }
+    
+    Trainer:: switchPokemon(&Pokemon)
+    {
+         switchPokemon(&Pokemon a){
+             active=a
+             
+         
+}
+#endif
+    }

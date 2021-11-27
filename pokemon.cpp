89721e6adcb15549d202572450b58fe30754ee80
ezpogue <ezpogue@gmail.com>
2021-11-26 21:28:25 -0800

    Added constructors

--- pokemon.cpp
+++ pokemon.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <utility>
 
+Pokemon(std::string name, Pokemon_Type type1, Pokemon_Type type2, Move* move1, Move* move2, Move* move3, Move* move4, int hp, int atk, int def, int spd):name(name), type.first(type1), type.second(type2), move[0](move1), move[1](move2), move[2](move3), move[3](move4), stats[0](hp), stats[1](atk), stats[2](def), stats[3](spd){}
+
 Move* Pokemon::useMove(int choice){return move[choice];}
 
 void Pokemon::catchMove(Move* a){
--- pokemon.hpp
+++ pokemon.hpp
@@ -14,6 +14,7 @@ protected:
 	int stats[4];// {HP,ATK,DEF,SPD}
 
 public:
+	Pokemon(std::string name, Pokemon_Type type1, Pokemon_Type type2, Move* move1, Move* move2, Move* move3, Move* move4, int hp, int atk, int def, int spd);
 	Move* useMove();
 	void catchMove(Move*);
 	std::pair<Pokemon_Type,Pokemon_Type> getType();

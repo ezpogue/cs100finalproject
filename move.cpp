21f6053ae4251525b60917b9b603cbc591ae4660
c9leb <flyaccgf@gmail.com>
2021-11-27 20:13:47 -0800

    Added more to move

--- move.cpp
+++ move.cpp
@@ -50,7 +50,108 @@ class NastyPlot(Pokemon pokemon) {
 
 class Flamethrower(Pokemon pokemon) {
 
-
+	power = 90;
+	pp  = 15;
+	priority = 0;
+	acc = 1;
 	
 }
 
+class Hurricane(Pokemon pokemon) {
+
+	power = 110;
+	acc  = .70;
+	pp = 10;
+	priority = 0;
+}
+
+class DragonClaw (Pokemon pokemon) {
+	power = 80;
+	pp = 15;
+	acc = 1;
+	priority = 0;
+}
+
+class BulletSeed (Pokemon pokemon) {
+	power = 75;
+	pp = 25;
+	acc = 1;
+	priority = 0;
+}
+
+class Earthquake (Pokemon pokemon) {
+	power = 100;
+	pp = 16;
+	acc = 1;
+	priority = 0;
+}
+
+class GigaDrain (Pokemon pokemon) {
+	power = 65;
+	pp = 24;
+	acc = 1;
+	priority = 0;
+}
+
+class LeafStorm (Pokemon pokemon) {
+	power = 120;
+	pp = 5;
+	acc = .7;
+	priority = 0;
+}
+
+class HydroPump (Pokemon pokemon) {
+	power = 120;
+	pp = 5;
+	acc = .7;
+	priority = 0;
+}
+
+class Scald (Pokemon pokemon) {
+	power = 90;
+	pp = 16;
+	acc = 1;
+	priority = 0;
+}
+
+class FireFang (Pokemon pokemon) {
+	power = 70;
+	pp =  24;
+	acc = 1;
+	priority = 0;
+}
+
+class QuickAttack (Pokemon pokemon) {
+	power = 40;
+	pp = 32;
+	acc = 1;
+	priority = 1;
+}
+
+class FireAttack (Pokemon pokemon) {
+	power = 120;
+	pp = 5;
+	acc = .7;
+	priority = 0;
+{
+
+class FlameCharge (Pokemon pokemon) {
+	power = 50;
+	pp = 32;
+	acc = 1;
+	priority = 0;
+}
+
+class Ember (Pokemon pokemon) {
+	power = 40;
+	pp = 32;
+	acc = 1;
+	priority = 0;
+}
+
+class ExtremeSpeed (Pokemon pokemon) {
+	power = 80;
+	pp = 5;
+	acc = 1;
+	priority = 1;
+}

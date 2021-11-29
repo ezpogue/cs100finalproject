41ed64bde78ccccbffad99b1a704dcaabe3172b4
ezpogue <ezpogue@gmail.com>
2021-11-29 04:54:11 -0800

    Overhaul of Move declarations

--- move.hpp
+++ move.hpp
@@ -3,87 +3,79 @@
 
 #include "type.hpp"
 #include "pokemon.hpp"
-
+#include <vector>
 class Move{ 
 
 protected: 
 	std::string name;
-public:
-	Move(std::string name);
-	std::string getName();
-};
-
-class Poke_Move : public Move {
-protected:
-	Type type;
-	int pp;
-	int acc;
+	Type* type;
+	int accuracy;
 	int priority;
+public:
+	virtual int getDamage();
+	virtual float getATKDebuff();
+	virtual float getDEFDebuff();
+	virtual float getSPDDebuff();
+	virtual float getATKBuff();
+	virtual float getDEFBuff();
+	virtual float getSPDBuff();
 };
 
 class Move_Set : public Move {
 protected:
-	Moves* move;
+	vector<Move*> movelist;
+public:
+	int getDamage();
+	float getATKDebuff();
+	float getDEFDebuff();
+	float getSPDDebuff();
+	float getATKBuff();
+	float getDEFBuff();
+	float getSPDBuff();
 };
 
-class Buff_Move : public Poke_Move {
+class Buff_Move : public Move {
 protected:
 	int stataffected;
 	int statchange;
-	int statmultiplier;
+	float statmultiplier;
 public:
-	void changeStat();
+	int getDamage();
+	float getATKDebuff();
+	float getDEFDebuff();
+	float getSPDDebuff();
+	float getATKBuff();
+	float getDEFBuff();
+	float getSPDBuff();
 };
 
-class Attack_Move : public Poke_Move {
+class Debuff_Move : public Move {
+protected:
+	int stataffected;
+	int statchange;
+	float statmultiplier;
+public:
+	int getDamage();
+	float getATKDebuff();
+	float getDEFDebuff();
+	float getSPDDebuff();
+	float getATKBuff();
+	float getDEFBuff();
+	float getSPDBuff();
+};
+
+class Attack_Move : public Move {
 protected:
 	int power;
 public:
-	void setPower(int p) { power = p; }
-	void setType(Type t) {type = t;}
-	void setAcc(int a) {acc = a;}
-	void setPriority(int pr) {priority = pr;}
+	int getDamage();
+	float getATKDebuff();
+	float getDEFDebuff();
+	float getSPDDebuff();
+	float getATKBuff();
+	float getDEFBuff();
+	float getSPDBuff();
 };
 
-class DragonDance : public Buff_Move {}
-
-class ShellSmash : public Buff_Move {}
-
-class Agility : public Buff_Move {}
-
-class TailWhip : public Buff_Move {}
-
-class NastyPlot : public Buff_Move {}
-/*
-class Flamethrower : public Attack_Move {}
-
-class Hurricane : public Attack_Move {}
-
-class DragonClaw : public Attack_Move {}
-
-class BulletSeed : public Attack_Move {}
-
-class Earthquake : public Attack_Move {}
-
-class GigaDrain : public Attack_Move {}
-
-class LeafStorm : public Attack_Move {}
-
-class HydroPump : public Attack_Move {}
-
-class Scald : public Attack_Move {}
-
-class FireFang : public Attack_Move {}
-
-class QuickAttack : public Attack_Move {}
-
-class FireBlast : public Attack_Move {}
-
-class FlameCharge : public Attack_Move {}
-
-class Ember : public Attack_Move {}
-
-class ExtremeSpeed : public Attack_Move {}
-*/
-
+#endif
 

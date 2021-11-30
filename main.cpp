90b0e9f62aec3b0c1227712fcf73e33378b8c18a
c9leb <flyaccgf@gmail.com>
2021-11-29 19:52:11 -0800

    fixed objects to match new move and pokemon

--- main.cpp
+++ main.cpp
@@ -2,184 +2,58 @@
 class Moves {
 public:
 	void Moves() {
-		Attack_Move Flamethrower;
-                Attack_Move Hurricane; 
-                Attack_Move DragonClaw; 
-                Attack_Move BulletSeed; 
-                Attack_Move Earthquake; 
-                Attack_Move GigaDrain; 
-                Attack_Move LeafStorm; 
-                Attack_Move FireFang; 
-                Attack_Move FireBlast; 
-                Attack_Move HydroPump; 
-                Attack_Move Scald;
-		Attack_Move QuickAttack;
-		Attack_Move FlameCharge;
-                Attack_Move Ember;
-                Attack_Move ExtremeSpeed;
-		
-		Flamethrower.setPower(90);
-		Hurricane.setPower(110);
-		Hurricane.setAcc(.7);
-		DragonClaw.setPower(80);
-		BulletSeed.setPower(75);
-		Earthquake.setPower(100);
-		GigaDrain.setPower(65);
-		LeafStorm.setPower(120);
-		LeafStorm.setAcc(.7);
-		HydroPump.setPower(120);
-		Scald.setPower(90);
-		FireFang.setPower(70);
-		QuickAttack.setPower(40);
-		QuickAttack.setPriority(1);
-		FireBlast.setPower(120);
-		FireBlast.setAcc(.7);
-		FlameCharge.setPower(50);
-		Ember.setPower(40);
-		ExtremeSpeed.setPower(80);
-		ExtremeSpeed.setPriority(1);
-		Flamethrower.setType(Fire);
-		Hurricane.setType(Flying);
-		DragonClaw.setType(Dragon);
-		BulletSeed.setType(Grass);
-		Earthquake.setType(Ground);
-		GigaDrain.setType(Grass);
-		LeafStorm.setType(Grass);
-		FireFang.setType(Fire);
-		FireBlast.setType(Fire);
-		HydroPump.setType(Water);
-		Scald.setType(Water);
-		QuickAttack.setType(Normal);
-		FlameCharge.setType(Fire);
-		Ember.setType(Fire);
-		ExtremeSpeed.setType(Normal);
-		
+		Move_Type* a = new Move_Type("Dark");
+		Attack_Move* Flamethrower = new Attack_Move("Flamethrower", new Fire_Move(), 100, 0, 90);
+                Attack_Move* Hurricane = new Attack_Move("Hurricane", new Flying_Move(), 70, 0, 110); 
+                Attack_Move* DragonClaw = new Attack_Move("Dragon Claw", new Dragon_Move(), 100, 0, 80); 
+                Attack_Move* BulletSeed = new Attack_Move("Bullet Seed", new Grass_Move(), 100, 0, 75); 
+                Attack_Move* Earthquake = new Attack_Move("Earthquake", new Ground_Move(), 100, 0, 100); 
+                Attack_Move* GigaDrain = new Attack_Move("Giga Drain", new Grass_Move(), 100, 0, 65); 
+                Attack_Move* LeafStorm = new Attack_Move("Leaf Storm", new Grass_Move(), 70, 0, 120); 
+                Attack_Move* FireFang = new Attack_Move("Fire Fang", new Fire_Move(), 100, 0, 70); 
+                Attack_Move* FireBlast = new Attack_Move("Fire Blast", new Fire_Move(), 70, 0, 120); 
+                Attack_Move* HydroPump = new Attack_Move("Hydro Pump", new Water_Move(), 70, 0, 120); 
+                Attack_Move* Scald = new Attack_Move("Scald", new Water_Move(), 100, 0, 90);
+		Attack_Move* QuickAttack = new Attack_Move("Quick Attack", new Normal_Move(), 100, 1, 40);
+		Attack_Move* FlameCharge = new Attack_Move("Flame Charge", new Fire_Move(),  100, 0, 50);
+                Attack_Move* Ember = new Attack_Move("Ember"_Move(), new Fire, 100, 0, 40);
+                Attack_Move* ExtremeSpeed = new Attack_Move("Extreme Speed", new Normal_Move(), 100, 1, 80);
+
+		Buff_Move* NastyPlot = new Buff_Move("Nasty Plot", a, 100, 0, 1, 2);
+		Debuff_Move* TailWhip = new DeBuff_Move("Tail Whip", a, 100, 0, 1, 2);
+		Move_Composite Shell_Smash("Shell Smash", a, 100, 0, Move* m);
+		Buff_Move* ss1 = new Buff_Move("ss1", a, 100, 1, 2);		
+                Buff_Move* ss2 = new Buff_Move("ss2", a, 100, 3, 2);
+		Debuff_Move* ss3 = new Debuff_Move("ss3", a, 100, 2, 2);
+		m.addMove(ss1);
+                m.addMove(ss2);
+                m.addMove(ss3);
+		Move_Composite DragonDance("Dragon Dance", a, 100, 0, Move* m1);
+                Buff_Move* dd1 = new Buff_Move("dd1", a, 100, 1, 1.5);
+                Buff_Move* dd2 = new Buff_Move("dd2", a, 100, 3, 1.5);
+		m1.addMove(dd1);
+		m1.addMove(dd2);
+		Buff_Move* Agility = new Buff_Move("Agility", a, 100, 0, 3, 2);
 	};
 }
 class Poke {
 public:
 	void Poke() {
-		Pokemon Charizard;
-		Pokemon Venusaur;
-		Pokemon Blastoise;
-		Pokemon Growlithe;
-		Pokemon Arcanine;
-		Pokemon Vulpix;
-		Pokemon Ninetales;
-		Pokemon Ponyta;
-		Pokemon Rapidash;
+		Pokemon_Type Fire("Fire");
+		Pokemon_Type Water("Water");
+		Pokemon_Type Grass("Grass");
+		Pokemon_Type Poison("Poison");
+		Pokemon_Type Flying("Flying");
 
-		Charizard.setName("Charizard");
-		Venusaur.setName("Venusaur");
-		Blastoise.setName("Blastoise");
-		Growlithe.setName("Growlithe");
-		Arcanine.setName("Arcanine");
-		Vulpix.setName("Vulpix");
-		Ninetales.setName("Ninetales");
-		Ponyta.setName("Ponyta");
-		Rapidash.setName("Rapidash");
-		
-		Charizard.setType(1, Fire);
-		Charizard.setType(2, Flying);
-                Venusaur.setType(1, Grass);
-		Venusaur.setType(2, Poison);
-		Blastoise.setType(1, Water);
-                Growlithe.setType(1, Fire);
-                Arcanine.setType(1, Fire);
-                Vulpix.setType(1, Fire);
-                Ninetales.setType(1, Fire);
-                Ponyta.setType(1, Fire);
-                Rapidash.setType(1, Fire);
-
-		Charizard.setMove(1, FireBlast);
-		Charizard.setMove(2, DragonClaw);
-		Charizard.setMove(3, Hurricane);
-		Charizard.setMove(4, buff);		
-
-		Blastoise.setMove(1, HydroPump);
-		Blastoise.setMove(2, Scald);
-		Blastoise.setMove(3, Earthquake);
-		Blastoise.setMove(4, buff);
-		
-		Venusaur.setMove(1, LeafStorm);
-		Venusaur.setMove(2, BulletSeed);
-		Venusaur.setMove(3, GigaDrain);
-		Venusaur.setMove(4, Earthquake);
-
-		Growlithe.setMove(1, Ember);
-		Growlithe.setMove(2, buff);
-		Growlithe.setMove(3, FireFang);
-		Growlithe.setMove(3, QuickAttack);
-
-		Arcanine.setMove(1, ExtremeSpeed);
-		Arcanine.setMove(2, FireBlast);
-		Arcanine.setMove(3, FireFang);
-		Arcanine.setMove(4, buff);
-
-		Vulpix.setMove(1, Ember);
-		Vulpix.setMove(2, Flamethrower);
-		Vulpix.setMove(3, buff);
-		Vulpix.setMove(4, FireBlast);
-		
-		Ninetales.setMove(1, Flamethrower);
-                Ninetales.setMove(2, FireBlast);
-                Ninetales.setMove(3, QuickAttack);
-                Ninetales.setMove(4, buff);
-
-		Ponyta.setMove(1, Ember);
-                Ponyta.setMove(2, Flamethrower);
-                Ponyta.setMove(3, buff);
-                Ponyta.setMove(4, FlameCharge);
-
-		Rapidash.setMove(1, FireBlast);
-                Rapidash.setMove(2, Flamethrower);
-                Rapidash.setMove(3, buff);
-                Rapidash.setMove(4, QuickAttack);
-
-		Charizard.setStat(1, 121);
-		Charizard.setStat(2, 82);
-		Charizard.setStat(3, 77);
-		Charizard.setStat(4, 95);
-
-		Venusaur.setStat(1, 117);
-                Venusaur.setStat(2, 77);
-                Venusaur.setStat(3, 83);
-                Venusaur.setStat(4, 75);
-
-		Blastoise.setStat(1, 125);
-		Blastoise.setStat(2, 83);
-		Blastoise.setStat(3, 97);
-		Blastoise.setStat(4, 79);
-
-		Vulpix.setStat(1, 88);
-		Vulpix.setStat(2, 47);
-                Vulpix.setStat(3, 47);
-                Vulpix.setStat(4, 66);
-
-                Ninetales.setStat(1, 124);
-		Ninetales.setStat(2, 95);
-		Ninetales.setStat(3, 93);
-		Ninetales.setStat(4, 110);
-
-		Growlithe.setStat(1, 106);
-		Growlithe.setStat(2, 73);
-		Growlithe.setStat(3, 53);
-		Growlithe.setStat(4, 65);
-		
-		Arcanine.setStat(1, 131);
-		Arcanine.setStat(2, 102);
-		Arcanine.setStat(3, 79);
-		Arcanine.setStat(4, 91)
-
-                Ponyta.setStat(1, 106);
-                Ponyta.setStat(2, 85);
-                Ponyta.setStat(3, 61);
-                Ponyta.setStat(4, 89);
-        
-                Rapidash.setStat(1, 106);
-                Rapidash.setStat(2, 90);
-                Rapidash.setStat(3, 68);
-                Rapidash.setStat(4, 94);;
+		Pokemon Rapidash("Rapidash", Fire, Fire, FireBlast, Flamethrower, TailWhip, QuickAttack, 106, 90, 68, 94);
+		Pokemon Ponyta("Ponyta", Fire, Fire, Ember, FlameCharge, Agility, TailWhip, 106, 85, 61, 89);
+		Pokemon Arcanine("Arcanine", Fire, Fire, FireBlast, Flamethrower, Agility, ExtremeSpeed, 131, 102, 79, 91);
+		Pokemon Growlithe("Growlithe", Fire, Fire, Ember, QuickAttack, FireFang, QuickAttack, 106, 73, 53, 65);
+		Pokemon Ninetales("Ninetales", Fire, Fire, FireBlast, Flamethrower, NastyPlot, QuickAttack, 124, 95, 93, 110);
+		Pokemon Vulpix("Vulpix", Fire, Fire, Ember, Flamethrower, TailWhip, QuickAttack, 88, 47, 47, 66);
+		Pokemon Charizard("Charizard", Fire, Flying, FireBlast, DragonClaw, DragonDance, Hurricane, 121, 82, 77, 95);
+		Pokemon Venusaur("Venusaur", Grass, Poison, LeafStorm, BulletSeed, GigaDrain, Earthquake, 117, 77, 83, 75);
+		Pokemon Blastoise("Blastoise", Water, Water, HydroPump, Scald, Earthquake, ShellSmash, 125, 83, 97, 79);
 	};
 }
 
